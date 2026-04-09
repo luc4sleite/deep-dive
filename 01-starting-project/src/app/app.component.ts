39a2cdd9789ff1be9ee052cdf4795631250e6de3
Lucas Gonçalves Leite <lucahleite@gmail.com>
2026-04-09 20:46:32 -0300

    feat: implement dashboard item component for modular dashboard display

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -3,12 +3,18 @@ import { HeaderComponent } from './header/header.component';
 import { ServerStatusComponent } from './dashboard/server-status/server-status.component';
 import { TrafficComponent } from './dashboard/traffic/traffic.component';
 import { SupportTicketsComponent } from './dashboard/support-tickets/support-tickets.component';
+import { DashboardItemComponent } from './dashboard/dashboard-item/dashboard-item.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent, SupportTicketsComponent],
+  imports: [
+    HeaderComponent,
+    ServerStatusComponent,
+    TrafficComponent,
+    SupportTicketsComponent,
+    DashboardItemComponent,
+  ],
   templateUrl: './app.component.html',
 })
-export class AppComponent {
-}
+export class AppComponent {}
