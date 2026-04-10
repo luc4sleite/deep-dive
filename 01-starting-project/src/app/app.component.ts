dd194f6bfbc240ba54a737457ddcbaf1022cbcc5
Lucas Gonçalves Leite <lucahleite@gmail.com>
2026-04-09 21:08:20 -0300

    feat: replace support tickets component with tickets component and add new ticket functionality

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { ServerStatusComponent } from './dashboard/server-status/server-status.component';
 import { TrafficComponent } from './dashboard/traffic/traffic.component';
-import { SupportTicketsComponent } from './dashboard/support-tickets/support-tickets.component';
+import { TicketsComponent } from './dashboard/tickets/tickets.component';
 import { DashboardItemComponent } from './dashboard/dashboard-item/dashboard-item.component';
 
 @Component({
@@ -12,7 +12,7 @@ import { DashboardItemComponent } from './dashboard/dashboard-item/dashboard-ite
     HeaderComponent,
     ServerStatusComponent,
     TrafficComponent,
-    SupportTicketsComponent,
+    TicketsComponent,
     DashboardItemComponent,
   ],
   templateUrl: './app.component.html',
